734c89603e870bfafeb636841a2851d02654bcf3
Kasandra  Kleespie <87586209+kasiix3@users.noreply.github.com>
2021-11-17 13:50:02 -0600

    Create program.cs

---
+++ program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      //let's build a ghost!
+      Console.WriteLine(" .-.");    
+      Console.WriteLine("(o o)");   
+      Console.WriteLine("| 0 |");  
+       Console.WriteLine("|   |"); 
+        Console.WriteLine("`~~~`"); 
+     }
+  }
+}
